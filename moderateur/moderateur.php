0e35191cffd285b7429d9dfb05172cb5d7409bd8
NEC720 <necjunana@gmail.com>
2024-04-24 16:57:21 +0100

    Blocage d' utilisateur

--- moderateur/moderateur.php
+++ moderateur/moderateur.php
@@ -2,3 +2,4 @@
 
 echo "Developpement de la fonction permettant de modeliser des messages";
 echo "Developpement de la fonction permettant de signaler un utilisateur";
+echo "Developpement de la fonction permettant de bloquer et debloquer un utilisateur";
